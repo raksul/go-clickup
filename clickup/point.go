d32bcd835bff143b4666e8480afdafe37dd5bf6d
Shingo Ichijima <s.ichijima.1991@gmail.com>
2022-11-27 17:35:50 +0900

    Use json.Number

--- clickup/point.go
+++ clickup/point.go
@@ -2,49 +2,13 @@ package clickup
 
 import (
 	"encoding/json"
-	"strconv"
 )
 
 type Point struct {
-	Value json.RawMessage
+	Value json.Number
 
 	IntVal   *int64
 	FloatVal *float64
-}
-
-func (p *Point) String() (val string) {
-	if p.IntVal != nil {
-		val = strconv.FormatInt(*p.IntVal, 10)
-		return
-	}
-
-	if p.FloatVal != nil {
-		val = strconv.FormatFloat(*p.FloatVal, 'f', -1, 64) //-1 will remove unimportant 0s
-		return
-	}
-
-	val = string(p.Value)
-	return
-}
-
-func (p *Point) Int() (val int64) {
-	if p.IntVal != nil {
-		val = (*(p.IntVal))
-	} else if p.FloatVal != nil {
-		val = int64(*p.FloatVal)
-	}
-
-	return
-}
-
-func (p *Point) Float() (val float64) {
-	if p.FloatVal != nil {
-		val = (*(p.FloatVal))
-	} else if (p.IntVal) != nil {
-		val = float64(*p.IntVal)
-	}
-
-	return
 }
 
 func (p *Point) MarshalJSON() ([]byte, error) {
@@ -56,24 +20,25 @@ func (p *Point) MarshalJSON() ([]byte, error) {
 		return json.Marshal(p.FloatVal)
 	}
 
-	return json.Marshal(p.Value) //default raw message
+	return json.Marshal(p.Value)
 }
 
-func (p *Point) UnmarshalJSON(b []byte) (err error) {
+func (p *Point) UnmarshalJSON(b []byte) error {
 	p.IntVal = nil
 	p.FloatVal = nil
 
 	var i int64
-	if e := json.Unmarshal(b, &i); e == nil {
-		p.IntVal = &i
-		return
-	}
 	var f float64
-	if e := json.Unmarshal(b, &f); e == nil {
-		p.FloatVal = &f
-		return
+	if err := json.Unmarshal(b, &i); err == nil {
+		p.IntVal = &i
+	} else {
+		if err = json.Unmarshal(b, &f); err == nil {
+			p.FloatVal = &f
+		} else {
+			return err
+		}
 	}
 
-	p.Value = json.RawMessage(b) //default to raw message
-	return
+	p.Value = json.Number(b)
+	return nil
 }
--- clickup/point_test.go
+++ clickup/point_test.go
@@ -6,56 +6,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_String(t *testing.T) {
-	r := require.New(t)
-
-	table := []struct {
-		P   Point
-		Exp string
-	}{
-		{Point{IntVal: Int64(9)}, "9"},
-		{Point{FloatVal: Float64(0.5)}, "0.5"},
-	}
-
-	for _, tt := range table {
-		r.Equal(tt.Exp, tt.P.String())
-	}
-}
-
-func Test_Int(t *testing.T) {
-	r := require.New(t)
-
-	table := []struct {
-		P   Point
-		Exp int64
-	}{
-		{Point{IntVal: Int64(9)}, 9},
-		{Point{FloatVal: Float64(0.5)}, 0},
-		{Point{FloatVal: Float64(1.5)}, 1},
-	}
-
-	for _, tt := range table {
-		r.Equal(tt.Exp, tt.P.Int())
-	}
-}
-
-func Test_Float(t *testing.T) {
-	r := require.New(t)
-
-	table := []struct {
-		P   Point
-		Exp float64
-	}{
-		{Point{IntVal: Int64(9)}, 9.0},
-		{Point{FloatVal: Float64(0.5)}, 0.5},
-		{Point{FloatVal: Float64(1.5)}, 1.5},
-	}
-
-	for _, tt := range table {
-		r.Equal(tt.Exp, tt.P.Float())
-	}
-}
-
 func Test_UnmarshalJSON(t *testing.T) {
 	r := require.New(t)
 
@@ -73,7 +23,7 @@ func Test_UnmarshalJSON(t *testing.T) {
 	for _, tt := range table {
 		err := p.UnmarshalJSON([]byte(tt.Val))
 		r.NoError(err)
-		r.Equal(tt.Exp, p.String())
+		r.Equal(tt.Exp, p.Value.String())
 	}
 }
 
