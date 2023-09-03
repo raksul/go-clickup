91852e4f26ffea7150d0785fd88325079a78c59e
Lucas Pearson <catdevman@gmail.com>
2023-09-03 13:00:13 -0400

    Added GetSeats and GetPlan (funcs, tests, and examples)
    Fixed GetTeams test calling GetAuthorizedTeams

--- clickup/teams_test.go
+++ clickup/teams_test.go
@@ -40,7 +40,7 @@ func TestTeamsService_GetTeams(t *testing.T) {
 	})
 
 	ctx := context.Background()
-	artifacts, _, err := client.Authorization.GetAuthorizedTeams(ctx)
+	artifacts, _, err := client.Teams.GetTeams(ctx)
 	if err != nil {
 		t.Errorf("Actions.ListArtifacts returned error: %v", err)
 	}
@@ -63,6 +63,83 @@ func TestTeamsService_GetTeams(t *testing.T) {
 		Members: []TeamMember{m},
 	}
 	want = append(want, team)
+	if !cmp.Equal(artifacts, want) {
+		t.Errorf("Actions.ListArtifacts returned %+v, want %+v", artifacts, want)
+	}
+}
+
+func TestTeamsService_GetSeats(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/team/123/seats", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		fmt.Fprint(
+            w,
+            `{
+                "members": {
+                  "filled_members_seats": 9,
+                  "total_member_seats": 9,
+                  "empty_member_seats": 0
+                },
+                "guests": {
+                  "filled_guest_seats": 2,
+                  "total_guest_seats": 50,
+                  "empty_guest_seats": 48
+                }
+            }`,
+		)
+	})
+
+	ctx := context.Background()
+	artifacts, _, err := client.Teams.GetSeats(ctx, "123")
+	if err != nil {
+		t.Errorf("Actions.ListArtifacts returned error: %v", err)
+	}
+
+	var want Seats
+    members := Members{
+        FilledMembersSeats: 9,
+        TotalMemberSeats: 9,
+        EmptyMemberSeats: 0,
+    }
+    guests := Guests{
+        FilledGuestSeats: 2,
+        TotalGuestSeats: 50,
+        EmptyGuestSeats: 48,
+    }
+    want.Members = members
+    want.Guests = guests
+	if !cmp.Equal(artifacts, want) {
+		t.Errorf("Actions.ListArtifacts returned %+v, want %+v", artifacts, want)
+	}
+}
+
+func TestTeamsService_GetPlan(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/team/123/plan", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		fmt.Fprint(
+            w,
+            `{
+                "plan_name": "Free Forever",
+                "plan_id": 1
+            }`,
+		)
+	})
+
+	ctx := context.Background()
+	artifacts, _, err := client.Teams.GetPlan(ctx, "123")
+	if err != nil {
+		t.Errorf("Actions.ListArtifacts returned error: %v", err)
+	}
+
+    want := Plan{
+        Id: 1,
+        Name: "Free Forever",
+    }
 	if !cmp.Equal(artifacts, want) {
 		t.Errorf("Actions.ListArtifacts returned %+v, want %+v", artifacts, want)
 	}
